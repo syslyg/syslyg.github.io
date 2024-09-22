83946ee5963c99fae9bc230b3863e702b9d3bd5d
SirDna63 <ritz.andras63@gmail.com>
2024-09-22 22:11:12 +0200

    működik a terv

---
+++ script.js
@@ -0,0 +1,12 @@
+// JavaScript functions for button actions 
+function buttonOneFunction() { 
+    document.getElementById("xd").innerHTML = "Vágjunk is bele"; 
+} 
+
+function buttonTwoFunction() { 
+    document.getElementById("xd").innerHTML = "hihi"; 
+} 
+
+function buttonThreeFunction() { 
+    document.getElementById("xd").innerHTML = "VÉGRE"; 
+} 
