4af43a000895722c57bacfb1a6dce99fc455e33c
SirDna63 <ritz.andras63@gmail.com>
2024-09-29 12:21:50 +0200

    alakul

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // JavaScript functions for button actions 
 function buttonOneFunction() { 
-    document.getElementById("xd").innerHTML = "Vágjunk is bele"; 
+    document.getElementById("xd").innerHTML = "én"; 
 } 
 
 function buttonTwoFunction() { 
-    document.getElementById("xd").innerHTML = "hihi"; 
+    document.getElementById("xd").innerHTML = "labdaaa"; 
 } 
 
 function buttonThreeFunction() { 
-    document.getElementById("xd").innerHTML = "VÉGRE"; 
+    document.getElementById("xd").innerHTML = "nyomod"; 
 } 
