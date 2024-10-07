2cc3e8c723126ec036d86e829804b7a56999a22b
SirDna63 <ritz.andras63@gmail.com>
2024-10-07 18:49:23 +0200

    szenvedés pt2

--- script.js
+++ script.js
@@ -7,17 +7,24 @@ function loadTextFromFile(filePath, elementId) {
         })
         .catch(error => console.error('Error loading file:', error));
 }
+function loadImage(imagePath, elementId) {
+    document.getElementById(elementId).src = imagePath;
+}
 
 // Button functions to load different text files
 function buttonOneFunction() { 
     document.getElementById("1").innerHTML = "Kedves Látogató!"
     loadTextFromFile('content1.txt', '2');
-    document.getElementById("3").innerHTML = "ui.: A fülek között lehet átfedés, érdemes sorban haladni."
+    document.getElementById("3").innerHTML = ""
+    loadImage("pic1.jpg", '4')
+    loadImage("pic1.jpg", "5")
 } 
 
 function buttonTwoFunction() { 
     loadTextFromFile('content2.txt', '2');
+    document.getElementById("1").innerHTML = "Versek";
     document.getElementById("3").innerHTML = ""
+
 } 
 
 function buttonThreeFunction() { 
