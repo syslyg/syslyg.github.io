fe7526f53dfd20f7baab1eaeca959e378877c9f8
SirDna63 <ritz.andras63@gmail.com>
2024-10-08 17:46:40 +0200

    mindjárt

--- script.js
+++ script.js
@@ -16,15 +16,41 @@ function buttonOneFunction() {
     loadImage("pic1.jpg", '4')
     loadImage("pic1.jpg", "5")
 } 
-
 function buttonTwoFunction() { 
     loadTextFromFile('content2.txt', '2');
     document.getElementById("1").innerHTML = "Versek";
     document.getElementById("3").innerHTML = ""
 
 } 
-
 function buttonThreeFunction() { 
     loadTextFromFile('content3.txt', '2');
+    document.getElementById("3").innerHTML = ""
+}
+function buttonFourFunction() { 
+    loadTextFromFile('content4.txt', '2');
+    document.getElementById("3").innerHTML = ""
+}
+function buttonFiveFunction() { 
+    loadTextFromFile('content5.txt', '2');
+    document.getElementById("3").innerHTML = ""
+}
+function buttonSixFunction() { 
+    loadTextFromFile('content6.txt', '2');
+    document.getElementById("3").innerHTML = ""
+}
+function buttonSevenFunction() { 
+    loadTextFromFile('content7.txt', '2');
+    document.getElementById("3").innerHTML = ""
+}
+function buttonEightFunction() { 
+    loadTextFromFile('content8.txt', '2');
+    document.getElementById("3").innerHTML = ""
+}
+function buttonNineFunction() { 
+    loadTextFromFile('content9.txt', '2');
+    document.getElementById("3").innerHTML = ""
+}
+function buttonTenFunction() { 
+    loadTextFromFile('content10.txt', '2');
     document.getElementById("3").innerHTML = ""
 }
