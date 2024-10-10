3ec04fc8982aa26d302ecd2aa781166993f8c3a5
SirDna63 <ritz.andras63@gmail.com>
2024-10-10 22:35:57 +0200

    mennyi van még?

--- script.js
+++ script.js
@@ -11,60 +11,60 @@ function loadImage(imagePath, elementId) {
 }
 function buttonOneFunction() { 
     document.getElementById("1").innerHTML = "Kedves Látogató!";
-    loadTextFromFile("content1.txt", "2");
+    loadTextFromFile("txt/content1.txt", "2");
     loadImage("pic1.jpg", '4');
     loadImage("pic1.jpg", "5");
 } 
 function buttonTwoFunction() { 
-    loadTextFromFile('content2.txt', '2');
+    loadTextFromFile('txt/content2.txt', '2');
     document.getElementById("1").innerHTML = "Versek";
     loadImage("pic1.jpg", '4');
     loadImage("pic1.jpg", "5");
 } 
 function buttonThreeFunction() { 
-    loadTextFromFile('content3.txt', '2');
+    loadTextFromFile('txt/content3.txt', '2');
     document.getElementById("1").innerHTML = "";
     loadImage("pic1.jpg", '4');
     loadImage("pic1.jpg", "5");
 }
 function buttonFourFunction() { 
-    loadTextFromFile('content4.txt', '2');
+    loadTextFromFile('txt/content4.txt', '2');
     document.getElementById("1").innerHTML = "";
     loadImage("pic1.jpg", '4');
     loadImage("pic1.jpg", "5");
 }
 function buttonFiveFunction() { 
-    loadTextFromFile('content5.txt', '2');
+    loadTextFromFile('txt/content5.txt', '2');
     document.getElementById("1").innerHTML = "";
     loadImage("pic1.jpg", '4');
     loadImage("pic1.jpg", "5");
 }
 function buttonSixFunction() { 
-    loadTextFromFile('content6.txt', '2');
+    loadTextFromFile('txt/content6.txt', '2');
     document.getElementById("1").innerHTML = "";
     loadImage("pic1.jpg", '4');
     loadImage("pic1.jpg", "5");
 }
 function buttonSevenFunction() { 
-    loadTextFromFile('content7.txt', '2');
+    loadTextFromFile('txt/content7.txt', '2');
     document.getElementById("1").innerHTML = "";
     loadImage("pic1.jpg", '4');
     loadImage("pic1.jpg", "5");
 }
 function buttonEightFunction() { 
-    loadTextFromFile('content8.txt', '2');
+    loadTextFromFile('txt/content8.txt', '2');
     document.getElementById("1").innerHTML = "";
     loadImage("pic1.jpg", '4');
     loadImage("pic1.jpg", "5");
 }
 function buttonNineFunction() { 
-    loadTextFromFile('content9.txt', '2');
+    loadTextFromFile('txt/content9.txt', '2');
     document.getElementById("1").innerHTML = "";
     loadImage("pic1.jpg", '4');
     loadImage("pic1.jpg", "5");
 }
 function buttonTenFunction() { 
-    loadTextFromFile('content10.txt', '2');
+    loadTextFromFile('txt/content10.txt', '2');
     document.getElementById("1").innerHTML = "";
     loadImage("pic1.jpg", '4');
     loadImage("pic1.jpg", "5");
